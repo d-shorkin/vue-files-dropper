56f7895269e1a6653ef01f7379b4faa66ea9a793
Дмитрий Шоркин <d.shorkin@gmail.com>
2018-07-07 17:26:49 +0700

    Change directories

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,15 @@
     <h1>Vue files dropper</h1>
     <div class="group">
       <vue-files-dropper v-model="files" multiple class="test" @change="changed++">
-        <div v-if="!files.length"><small>Custom multiple input</small><br/>Drop files here</div>
+        <div v-if="!files.length">
+          <small>Custom multiple input</small>
+          <br/>Drop files here
+        </div>
         <div v-else>
           <span>You added {{files.length}} files</span>
-          <div v-for="(file, index) in this.files" :key="index"><small>{{file.name}}</small></div>
+          <div v-for="(file, index) in this.files" :key="index">
+            <small>{{file.name}}</small>
+          </div>
         </div>
       </vue-files-dropper>
     </div>
@@ -28,28 +33,30 @@
 </template>
 
 <script>
+  import './components/'
 
-export default {
-  name: 'app',
-  watch:{
-    files(files){
-      console.log(files)
-    }
-  },
-  data: ()=>({
-    files: [],
-    file: null,
-    changed: 0
-  }),
-}
+  export default {
+    name: 'app',
+    watch: {
+      files(files) {
+        console.log(files)
+      }
+    },
+    data: () => ({
+      files: [],
+      file: null,
+      changed: 0
+    }),
+  }
 </script>
 
 <style lang="scss">
-  body{
+  body {
     margin: 0;
     padding: 0;
 
   }
+
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -59,18 +66,19 @@ export default {
     max-width: 400px;
     margin: 0 auto;
 
-    h1{
+    h1 {
       text-align: center;
     }
   }
-  .test{
+
+  .test {
     background: aqua;
     border-color: darken(aqua, 5);
     min-height: 10em;
     transition: 1s;
 
-    &:not(.disabled){
-      &:hover, &.hover{
+    &:not(.disabled) {
+      &:hover, &.hover {
         background: chocolate !important;
         color: white;
         border-color: darken(chocolate, 5);
@@ -78,7 +86,8 @@ export default {
       }
     }
   }
-  .group{
+
+  .group {
     margin-bottom: 1em;
   }
 </style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+import Vue from 'vue';
 import VueFilesDropper from './VueFilesDropper'
+
+Vue.component('VueFilesDropper', VueFilesDropper);
 
 export default {VueFilesDropper}
